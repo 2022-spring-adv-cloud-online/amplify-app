37b667e2a3137d4c13671fe21eaef782bfa2ebe3
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-01-26 11:21:10 -0600

    Did an amplify push...
    Don't know why I got a TS file here ? ? ?

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp22s04cf6ba3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
