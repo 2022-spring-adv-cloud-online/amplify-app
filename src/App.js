4d00da7843bc75a6c28cff67e8b6ad7ccf4956e2
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-01-27 11:58:17 -0600

    Actually, maybe skip withAuthenticator() and...
    Just style this one up a bit. I believe the <Authenticator /> component is the more expected technique.
    
    And I couldn't get withAuthenticator() working anyhow : - O

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 //   withAuthenticator
 // } from '@aws-amplify/ui-react';
 
-import { AmplifyProvider, Authenticator } from '@aws-amplify/ui-react';
+
+import { AmplifyProvider, Authenticator, Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 const App = () => {
@@ -17,11 +18,11 @@ const App = () => {
               <h1>
                 Hello {user?.username}, from AWS Amplify
               </h1>
-              <button
+              <Button
                 onClick={signOut}
               >
                 Sign Out
-              </button>
+              </Button>
             </>
           )
         }
